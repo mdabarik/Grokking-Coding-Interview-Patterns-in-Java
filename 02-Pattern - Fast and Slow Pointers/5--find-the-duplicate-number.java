2c21b700c9222568d273be1a338814f63a0da8ee
mdabarik <mdabarik19@gmail.com>
2023-07-22 17:41:03 +0600

    fast and slow pointers added

--- 02-Pattern - Fast and Slow Pointers/5--find-the-duplicate-number.java
+++ 02-Pattern - Fast and Slow Pointers/5--find-the-duplicate-number.java
@@ -1,5 +1,37 @@
-package 02-Pattern - Fast and Slow Pointers;
+/*
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
 
-public class 5--find-the-duplicate-number {
-    
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+Example 1:
+Input: nums = [1,3,4,2,2]
+Output: 2
+
+Example 2:
+Input: nums = [3,1,3,4,2]
+Output: 3
+*/
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int tortoise = nums[0];
+        int hare = nums[0];
+        
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+        
+        tortoise = nums[0];
+        while (tortoise != hare) {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
+        }
+        
+        return tortoise;
+    }
 }
+
+// TC: O(n), SC: O(1)
--- 02-Pattern - Fast and Slow Pointers/6--palindrome-linked-list.java
+++ 02-Pattern - Fast and Slow Pointers/6--palindrome-linked-list.java
@@ -1,5 +1,50 @@
-package 02-Pattern - Fast and Slow Pointers;
+/*
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
 
-public class 6--palindrome-linked-list {
-    
+Example 1:
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+Input: head = [1,2]
+Output: false
+
+Follow up: Could you do it in O(n) time and O(1) space?
+*/
+
+class Solution {
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode tmp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) return true;
+        if (head.next == null) return true;
+        if (head.next.next == null) return head.val == head.next.val;
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode head1 = head, head2 = slow.next;
+        slow.next = null;
+        head2 = reverse(head2);
+        while (head2 != null) {
+            if (head1.val != head2.val) {
+                return false;
+            }
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+        return true;
+    }
 }
+
+// TC: O(n), SC: O(1)
