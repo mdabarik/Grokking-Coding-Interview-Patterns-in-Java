eeb1bfaac24c626b50f0d919519abdf842e38324
mdabarik <mdabarik19@gmail.com>
2023-07-27 19:51:13 +0600

    new pattern added

---
+++ 19- Pattern - Tree Depth First Search/01--flatten-binary-tree-into-linked-list.java
@@ -0,0 +1,38 @@
+
+
+class Solution {
+    TreeNode prev = null;
+    public void flatten(TreeNode root) {
+        if (root == null)
+            return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+}
+
+// TC: O(n), SC: O(n)
+
+
+
+class Solution {
+    public static void flatten(Node root) {
+        Node curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+                Node last = curr.left;
+                while (last.right != null) {
+                    last = last.right;
+                }
+                last.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+            curr = curr.right;
+        }
+    }
+}
+
+// TC: O(n), SC: O(1)
